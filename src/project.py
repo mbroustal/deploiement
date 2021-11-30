01f4f0c157687209bd939948c450a4601dd1b66a
mbroustal <62555192+mbroustal@users.noreply.github.com>
2021-11-30 09:04:17 +0100

    Update project.py

--- src/project.py
+++ src/project.py
@@ -1,11 +1,12 @@
 def hello():
-  "doc_string"
+  """ Returns "Hello, world!" """
   print('Hello, world!')
   return
 
 def add_2(int a):
-  "doc_string"
+  """ Add 2 to a number """
   return a+2
 
 def is_even(val):
+  """ Returns True if the input number is even, False if it's odd """
   return num % 2 == 0
