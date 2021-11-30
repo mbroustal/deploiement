e1d4f52f575a4d9d3b5d9483660b84dd13ba61fb
mbroustal <62555192+mbroustal@users.noreply.github.com>
2021-11-30 09:25:50 +0100

    Update project.py

--- src/project.py
+++ src/project.py
@@ -4,9 +4,31 @@ def hello():
   return
 
 def add_2(int a):
-  """ Add 2 to a number """
+  """ 
+  Add 2 to a number 
+  
+  Parameters
+  --------------
+    a : int
+      Number to add
+      
+  Returns
+  ------------
+    int, value a+2
+  """
   return a+2
 
 def is_even(val):
-  """ Returns True if the input number is even, False if it's odd """
+  """ 
+  Returns True if the input number is even, False if it's odd 
+  
+  Parameters
+  -------------
+    val : int
+      Number to evaluate
+      
+  Returns
+  -------------
+    boolean, True if val is even, False if val is odd
+  """
   return num % 2 == 0
